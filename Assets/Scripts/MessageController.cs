1e2e25402e9fe7aaa94daf5cbd7c7872f870163c
Soner Soykan ÇALI <soner.cali@ogr.dpu.edu.tr>
2019-09-24 13:56:40 +0300

    network connection is established

---
+++ Assets/Scripts/MessageController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Newtonsoft.Json;
+public class MessageController : MonoBehaviour
+{
+    public static MessageController inst;
+    public TextMeshProUGUI textmesh;
+    string recMessage;
+    private void Awake()
+    {
+        inst = this;
+    }
+    void Start()
+    {
+        
+       
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        recMessage = NetworkListener.instance.receivedString;
+        Debug.Log("message controller received string "+recMessage);
+        MessageInfo mes = JsonConvert.DeserializeObject<MessageInfo>(recMessage);
+        Debug.Log("***********************"+mes.getAlt().ToString());
+        textmesh.text = mes.getAlt().ToString();
+        // MessageController messagecontroller = JsonUtility.FromJson<MessageController>(recMessage);
+        //string a = JsonConvert.SerializeObject();
+
+
+
+        //  textmesh.transform.GetComponent<TextMeshProUGUI>().text = ""+mes.getLat().ToString() + mes.getLot() + mes.getSymbolIndex() ;
+
+
+    }
+
+    public class MessageInfo
+    {
+        public float lat;
+        public float lot;
+        public float alt;
+        public string name;
+        public int symbolIndex;
+
+        public MessageInfo(float lat, float lot, float alt, string name, int symbolIndex)
+        {
+            this.lat = lat;
+            this.lot = lot;
+            this.alt = alt;
+            this.name = name;
+            this.symbolIndex = symbolIndex;
+            ;
+        }
+        public MessageInfo() { }
+
+        public float getLat()
+        {
+            return lat;
+        }
+
+        public float getLot()
+        {
+            return lot;
+        }
+        public float getAlt()
+        {
+            return alt;
+        }
+        public string getName()
+        {
+            return name;
+        }
+        public int getSymbolIndex()
+        {
+            return symbolIndex;
+        }
+    }
+}
